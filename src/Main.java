d50e7c43c871767052325abcdeecc9e1ef79b21a
Anish <vu1f2425123@pvppcoe.ac.in>
2025-12-27 13:56:45 +0530

    1st

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,32 @@ class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("ENTER 1ST VARIABLE:");
-        double a = sc.nextDouble();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("ENTER 1ST VARIABLE:");
+//        double a = sc.nextDouble();
+//
+//        Scanner inp = new Scanner(System.in);
+//        System.out.print("ENTER 2ND VARIABLE:");
+//        double b = inp.nextDouble();
+//        double c = a * b;
+//        System.out.print("THE MULTIPLICATION OF 2 VARIABLE IS: ");
+//        System.out.println(c);
+//        System.out.println("Hello");
+//        System.out.println("KAISA HAI ANISH");
 
-        Scanner inp = new Scanner(System.in);
-        System.out.print("ENTER 2ND VARIABLE:");
-        double b = inp.nextDouble();
-        double c = a * b;
-        System.out.print("THE MULTIPLICATION OF 2 VARIABLE IS: ");
-        System.out.println(c);
-        System.out.println("Hello");
-        System.out.println("KAISA HAI ANISH");
+        System.out.println(3*2);
+        int num1=3;
+        System.out.println(num1*2);
+        int res = num1*2;
+        int amt = 10000;
+        float it=amt*0.05f;
+        System.out.println(it);
+
+
+
+
+
+
     }
 }
+
